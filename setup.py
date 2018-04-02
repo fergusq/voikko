af0f1f0fc93ba3e7df5862c1cf4af48c9c1e1f87
Iikka Hauhio <iikka@kaivos.org>
2018-04-02 20:17:14 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='voikko',
+      version='0.1',
+      description='Python 3 version of libvoikko and the word inflecting library',
+      url='http://github.com/fergusq/voikko',
+      author='Harri Pitkänen and Iikka Hauhio',
+      author_email='iikka.hauhio@gmail.com',
+      license='GPL',
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ],
+      packages=['voikko'],
+      package_data={
+          'voikko': ['sanat.txt', 'subst.aff', 'verb.aff'],
+      },
+      install_requires=[],
+      zip_safe=False)
